63ecef7c604aa6e175f7d9ca41307e0adc51ce3b
101hawn <101hawn@st-birinus.oxon.sch.uk>
2014-06-08 20:05:46 +0100

    Added shell

---
+++ Shell/Shell.rb
@@ -0,0 +1,27 @@
+require 'socket'
+
+begin
+	input = TCPSocket.new('localhost',5558)
+	output = TCPSocket.new('localhost',5555)
+rescue 
+	puts "error: #{$!}"
+else
+	@running = true
+	the = Thread.new{
+@gots = ''
+while @running do
+@gots = @gots + output.gets
+end
+}
+	command = ''
+	print ">>>"
+	until command.include?('exit') do
+	command = gets()
+	input.puts(command)
+	puts @gots
+	@gots = ''
+	print ">>>"
+	end
+	@running = false
+
+end
