be3a19fe437bede6e3b4a818efe4f6ad451f7274
101hawn <101hawn@st-birinus.oxon.sch.uk>
2014-06-15 12:17:40 +0100

    Now working

--- Inputs/dummy.py
+++ Inputs/dummy.py
@@ -15,13 +15,13 @@ while Library.alive():
 	#create an array
 	a = {}
 	#add data to array for transformation to c#
-	a["Roll"] = 0
+	a["Roll"] = 10
 	a["Roll_timeout"] = 1000
-	a["Yaw"] = 0
+	a["Yaw"] = 10
 	a["Yaw_timeout"] = 1000
-	a["Pitch"] = 0
+	a["Pitch"] = 10
 	a["Pitch_timeout"] = 1000
-	a["Altitude"] = 1
+	a["Altitude"] = 10
 	a["Altitude_timeout"] = 3000
 	
 	#print the json dumps to stdout
